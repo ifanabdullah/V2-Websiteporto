6daf98880e3df9282efacc9ad452ec2c026674dd
ifanabdullah <ifanbae807@gmail.com>
2023-11-19 09:03:45 +0700

    ok

--- src/sections/Contact.vue
+++ src/sections/Contact.vue
@@ -125,7 +125,7 @@ let submitContactForm = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.linkedin.com/in/fahim-anzam-dip/" target="_blank">
+                                        <a href="#" target="_blank">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" class="fill-current transition-all duration-300 hover:fill-secondary-500 dark:hover:fill-secondary-400">
                                                 <path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096 1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387 2.704 0 3.203 1.778 3.203 4.092v4.71z"></path>
                                             </svg>
